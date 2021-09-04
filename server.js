82d0078c1bfb45716ac0eb0b6c8f960cd404ff74
AshisPatel <ashis.n.patel@gmail.com>
2021-09-03 23:07:10 -0500

    Add election db to server.js

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 9001; 
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username, 
+        user: 'root',
+        // Your MySQL password
+        password: '$wampScriptwisp123',
+        database: 'election'
+    }, 
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
