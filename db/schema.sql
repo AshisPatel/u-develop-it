b1ac3c8d340e1a3a358cb8226f7702d6599784a7
AshisPatel <ashis.n.patel@gmail.com>
2021-09-04 19:10:02 -0500

    Add route to grab list of candidates based on votes

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties; 
 DROP TABLE IF EXISTS voters; 
-DROP TABLE IF EXISTS votes;
+
 
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -35,3 +36,5 @@ CREATE TABLE votes (
     CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
     CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
+
+
